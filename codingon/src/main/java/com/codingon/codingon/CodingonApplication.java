102ecc461d33768b4f5adba5e20771cefaf048ed
linda-spr <24you@spreatics.com>
2023-05-19 13:59:08 +0900

    String init

---
+++ codingon/src/main/java/com/codingon/codingon/CodingonApplication.java
@@ -0,0 +1,12 @@
+package com.codingon.codingon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodingonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodingonApplication.class, args);
+	}
+}
---
+++ codingon/src/main/java/com/codingon/codingon/controller/HelloController.java
@@ -0,0 +1,14 @@
+package com.codingon.codingon.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+    @GetMapping("/")
+    @ResponseBody
+    public String Hello() {
+        return "hi";
+    }
+}
